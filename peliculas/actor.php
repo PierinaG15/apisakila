d6b8085447094f71635b572391d3b1fb5139be47
PierinaG15 <pierinaagh7@gmail.comecho>
2025-02-12 20:21:02 +0100

    ultimas actualizaciones

--- peliculas/actor.php
+++ peliculas/actor.php
@@ -1,12 +1,12 @@
 <?php
 include("../conexiondb.php");
-$sql="select actor_id,first_name,last_name from actor where first_name=:first_name";
+$sql="SELECT F.film_id,F.title,F.description,F.release_year FROM film as F
+inner join film_actor as FA on F.film_id=FA.film_id
+where FA.actor_id=:actor_id;";
 $stm=$conexion->prepare($sql);
-$stm->bindParam(":first_name",$_GET['name']);
+$stm->bindParam(":actor_id",$_GET['id']);
 $stm->execute();
 $datos=$stm->fetchAll(PDO::FETCH_ASSOC);
 $json=json_encode($datos);
-// Establece la cabecera para indicar que la respuesta es JSON
-header('Content-Type: application/json');
-echo $json;
+include('../envio.php');
 ?>

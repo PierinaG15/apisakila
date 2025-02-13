1e143dffb2090c9513fc761b5b89d3b2631cdc4e
PierinaG15 <pierinaagh7@gmail.comecho>
2025-02-13 19:38:47 +0100

    comitiando cambios

--- actores/all.php
+++ actores/all.php
@@ -1,10 +1,8 @@
 <?php
 include("../conexiondb.php");
-$sql="select * from film";
+$sql="select actor_id,first_name,last_name from actor";
 $result=$conexion->query($sql);
 $datos=$result->fetchAll(PDO::FETCH_ASSOC);
 $json=json_encode($datos);
-// Establece la cabecera para indicar que la respuesta es JSON
-header('Content-Type: application/json');
-echo $json;
+include("../envio.php");    
 ?>
